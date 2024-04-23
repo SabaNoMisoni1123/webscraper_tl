e8dd1066d3aa7da6145c3415756bc0d51b893217
Sota Kondo <sota_kondo@mri.co.jp>
2024-04-23 17:32:36 +0900

    start vuetify

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-plugin-persistedstate'
+import { Vuetify } from 'vuetify/lib'
 
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App);
 const pinia = createPinia();
-pinia.use(createPersistedState());
 
-app.use(pinia)
-app.use(router)
+pinia.use(createPersistedState());
+const vuetify = Vuetify({});
+
+app.use(pinia);
+app.use(router);
+app.use(vuetify);
 
 app.mount('#app')
