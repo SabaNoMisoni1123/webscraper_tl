e2d7775f4c41f38dd26e84bceee7de09caea74f7
sabamiso <s.kondo11235813@gmail.com>
2025-04-20 03:16:16 +0900

    fix

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -2,7 +2,7 @@
   <div class="contact">
     <div class="contactBoddy">
       <h1>{{ AppConfig.appName }} v{{ AppConfig.version }}</h1>
-      <p>官公庁ウェブサイトから新着情報を収集し、ライムライン形式でまとめるサイトです。</p>
+      <p>官公庁ウェブサイトから新着情報を収集し、タイムライン形式でまとめるサイトです。</p>
       <p>詳しくは、本ウェブサイトコードのレポジトリ<a
           href="https://github.com/SabaNoMisoni1123/webscraper_tl">https://github.com/SabaNoMisoni1123/webscraper_tl</a>をご参照ください。
       </p>
